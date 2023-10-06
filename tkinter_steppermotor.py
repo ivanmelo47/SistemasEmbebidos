f9a06f04ad7f56c64366b6b29b5c5fd8c5099c08
ivanmelo47 <ivanmelo47@gmail.com>
2023-10-06 15:23:05 -0600

    J6

--- tkinter_steppermotor.py
+++ tkinter_steppermotor.py
@@ -1,74 +1,60 @@
 import tkinter as tk
-from RPi.GPIO import setmode, setup, output, BCM
+import RPi.GPIO as GPIO
 import time
 
-# Configuración de pines GPIO
-setmode(BCM)
-IN1 = 2  # Pin GPIO para IN1
-IN2 = 3  # Pin GPIO para IN2
-IN3 = 4  # Pin GPIO para IN3
-IN4 = 17  # Pin GPIO para IN4
+# Configura los pines GPIO
+IN1 = 2  # Pin IN1 de la placa ULN2003A (ajusta según tu configuración)
+IN2 = 3  # Pin IN2 de la placa ULN2003A (ajusta según tu configuración)
+IN3 = 4  # Pin IN3 de la placa ULN2003A (ajusta según tu configuración)
+IN4 = 5  # Pin IN4 de la placa ULN2003A (ajusta según tu configuración)
 
-# Configura los pines GPIO como salida
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
 
-# Secuencia de pasos para girar el motor en sentido horario
-sequence = [
-    (1, 0, 0, 0),
-    (1, 1, 0, 0),
-    (0, 1, 0, 0),
-    (0, 1, 1, 0),
-    (0, 0, 1, 0),
-    (0, 0, 1, 1),
-    (0, 0, 0, 1),
-    (1, 0, 0, 1),
-]
+# Crea una ventana de Tkinter
+window = tk.Tk()
+window.title("Control de Motor Paso a Paso")
 
-# Función para girar el motor en la dirección deseada
-def girar_motor(pasos):
-    for _ in range(pasos):
-        for step in sequence:
-            output(IN1, step[0])
-            output(IN2, step[1])
-            output(IN3, step[2])
-            output(IN4, step[3])
-            time.sleep(0.001)  # Ajusta la velocidad aquí
+# Función para mover el motor en sentido horario
+def move_clockwise():
+    GPIO.output(IN1, 1)
+    GPIO.output(IN2, 0)
+    GPIO.output(IN3, 0)
+    GPIO.output(IN4, 0)
+
+# Función para mover el motor en sentido antihorario
+def move_counterclockwise():
+    GPIO.output(IN1, 0)
+    GPIO.output(IN2, 0)
+    GPIO.output(IN3, 0)
+    GPIO.output(IN4, 1)
 
 # Función para detener el motor
-def detener_motor():
-    output(IN1, LOW)
-    output(IN2, LOW)
-    output(IN3, LOW)
-    output(IN4, LOW)
+def stop_motor():
+    GPIO.output(IN1, 0)
+    GPIO.output(IN2, 0)
+    GPIO.output(IN3, 0)
+    GPIO.output(IN4, 0)
 
-# Crear la ventana de la interfaz gráfica
-root = tk.Tk()
-root.title("Control de Motor Paso a Paso")
+# Crea los botones para controlar el motor
+clockwise_button = tk.Button(window, text="Sentido Horario", command=move_clockwise)
+clockwise_button.pack()
 
-# Crear botones para controlar el motor
-btn_horario = tk.Button(root, text="Sentido Horario")
-btn_antihorario = tk.Button(root, text="Sentido Antihorario")
-btn_detener = tk.Button(root, text="Detener")
+counterclockwise_button = tk.Button(window, text="Sentido Antihorario", command=move_counterclockwise)
+counterclockwise_button.pack()
 
-# Configurar las funciones de los botones
-def iniciar_sentido_horario(event):
-    girar_motor(1)  # Girar un paso en sentido horario
+stop_button = tk.Button(window, text="Detener", command=stop_motor)
+stop_button.pack()
 
-def iniciar_sentido_antihorario(event):
-    girar_motor(-1)  # Girar un paso en sentido antihorario
+# Función para detener el motor al cerrar la ventana
+def on_closing():
+    stop_motor()
+    GPIO.cleanup()
+    window.destroy()
 
-btn_horario.bind("<ButtonPress>", iniciar_sentido_horario)
-btn_antihorario.bind("<ButtonPress>", iniciar_sentido_antihorario)
-btn_detener.bind("<ButtonPress>", detener_motor)
+window.protocol("WM_DELETE_WINDOW", on_closing)
 
-# Colocar los botones en la ventana
-btn_horario.pack()
-btn_antihorario.pack()
-btn_detener.pack()
-
-# Iniciar la ventana principal
-root.mainloop()
+window.mainloop()
