03e3bcdd04a4d781fd00a47d6900cd0ac97b7b26
equipo5em <146138228+equipo5em@users.noreply.github.com>
2023-10-06 14:47:29 -0700

    Update tkinter_steppermotor.py

--- tkinter_steppermotor.py
+++ tkinter_steppermotor.py
@@ -6,7 +6,7 @@ import time
 IN1 = 2  # Pin IN1 de la placa ULN2003A (ajusta según tu configuración)
 IN2 = 3  # Pin IN2 de la placa ULN2003A (ajusta según tu configuración)
 IN3 = 4  # Pin IN3 de la placa ULN2003A (ajusta según tu configuración)
-IN4 = 5  # Pin IN4 de la placa ULN2003A (ajusta según tu configuración)
+IN4 = 17  # Pin IN4 de la placa ULN2003A (ajusta según tu configuración)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(IN1, GPIO.OUT)
@@ -14,30 +14,56 @@ GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
 
+#Secuencia de pasos para girar el motor en sentido horario
+sequence = [
+    (1, 0, 0, 0),
+    (1, 1, 0, 0),
+    (0, 1, 0, 0),
+    (0, 1, 1, 0),
+    (0, 0, 1, 0),
+    (0, 0, 1, 1),
+    (0, 0, 0, 1),
+    (1, 0, 0, 1),
+]
+
+#Secuencia en reversa
+sequence_ccw = list(reversed(sequence))
+
+# Calcula el número de pasos para una vuelta completa (360 grados)
+pasos_por_vuelta = 510  # Esto es para el motor 28BYJ-48
+
+# Define la velocidad (ajusta este valor para cambiar la velocidad)
+velocidad_s = 0.001  # Tiempo de espera en segundos entre pasos (mayor valor = menor velocidad)
+
 # Crea una ventana de Tkinter
 window = tk.Tk()
 window.title("Control de Motor Paso a Paso")
 
 # Función para mover el motor en sentido horario
 def move_clockwise():
-    GPIO.output(IN1, 1)
-    GPIO.output(IN2, 0)
-    GPIO.output(IN3, 0)
-    GPIO.output(IN4, 0)
+    # Gira el motor en sentido horario para una vuelta completa
+    for _ in range(pasos_por_vuelta):
+        for step in sequence:
+            GPIO.output(IN1, step[0])
+            GPIO.output(IN2, step[1])
+            GPIO.output(IN3, step[2])
+            GPIO.output(IN4, step[3])
+            time.sleep(velocidad_s)  # Ajusta la velocidad aquí
 
 # Función para mover el motor en sentido antihorario
 def move_counterclockwise():
-    GPIO.output(IN1, 0)
-    GPIO.output(IN2, 0)
-    GPIO.output(IN3, 0)
-    GPIO.output(IN4, 1)
+    for _ in range(pasos_por_vuelta):
+        for step in sequence_ccw:
+            GPIO.output(IN1, step[0])
+            GPIO.output(IN2, step[1])
+            GPIO.output(IN3, step[2])
+            GPIO.output(IN4, step[3])
+            time.sleep(velocidad_s)  # Ajusta la velocidad aquí
 
 # Función para detener el motor
 def stop_motor():
-    GPIO.output(IN1, 0)
-    GPIO.output(IN2, 0)
-    GPIO.output(IN3, 0)
-    GPIO.output(IN4, 0)
+    # Detiene el motor
+    GPIO.cleanup()
 
 # Crea los botones para controlar el motor
 clockwise_button = tk.Button(window, text="Sentido Horario", command=move_clockwise)
