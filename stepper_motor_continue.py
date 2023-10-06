3fe3617bc9397c3e066f3afb6f2425ad9eb63c9d
ivanmelo47 <ivanmelo47@gmail.com>
2023-10-06 15:52:20 -0600

    gta5

---
+++ stepper_motor_continue.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+import RPi.GPIO as GPIO
+import time
+
+# Configura los pines GPIO
+IN1 = 2  # Pin IN1 de la placa ULN2003A (ajusta según tu configuración)
+IN2 = 3  # Pin IN2 de la placa ULN2003A (ajusta según tu configuración)
+IN3 = 4  # Pin IN3 de la placa ULN2003A (ajusta según tu configuración)
+IN4 = 17  # Pin IN4 de la placa ULN2003A (ajusta según tu configuración)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+
+# Secuencia de pasos para girar el motor en sentido horario
+sequence = [
+    (1, 0, 0, 0),
+    (1, 1, 0, 0),
+    (0, 1, 0, 0),
+    (0, 1, 1, 0),
+    (0, 0, 1, 0),
+    (0, 0, 1, 1),
+    (0, 0, 0, 1),
+    (1, 0, 0, 1),
+]
+
+# Secuencia en reversa
+sequence_ccw = list(reversed(sequence))
+
+# Calcula el número de pasos para una vuelta completa (360 grados)
+pasos_por_vuelta = 510  # Esto es para el motor 28BYJ-48
+
+# Define la velocidad (ajusta este valor para cambiar la velocidad)
+velocidad_s = 0.001  # Tiempo de espera en segundos entre pasos (mayor valor = menor velocidad)
+
+# Crea una ventana de Tkinter
+window = tk.Tk()
+window.title("Control de Motor Paso a Paso")
+
+# Variables para rastrear el estado de los botones
+motor_en_marcha = False
+
+# Función para mover el motor en sentido horario
+def move_clockwise():
+    global motor_en_marcha
+    motor_en_marcha = True
+    step_counter = 0
+    while motor_en_marcha:
+        step = sequence[step_counter % len(sequence)]
+        GPIO.output(IN1, step[0])
+        GPIO.output(IN2, step[1])
+        GPIO.output(IN3, step[2])
+        GPIO.output(IN4, step[3])
+        step_counter += 1
+        time.sleep(velocidad_s)
+
+# Función para mover el motor en sentido antihorario
+def move_counterclockwise():
+    global motor_en_marcha
+    motor_en_marcha = True
+    step_counter = 0
+    while motor_en_marcha:
+        step = sequence_ccw[step_counter % len(sequence_ccw)]
+        GPIO.output(IN1, step[0])
+        GPIO.output(IN2, step[1])
+        GPIO.output(IN3, step[2])
+        GPIO.output(IN4, step[3])
+        step_counter += 1
+        time.sleep(velocidad_s)
+
+# Función para detener el motor
+def stop_motor():
+    global motor_en_marcha
+    motor_en_marcha = False
+
+# Crea los botones para controlar el motor
+clockwise_button = tk.Button(window, text="Sentido Horario", command=move_clockwise)
+clockwise_button.pack()
+
+counterclockwise_button = tk.Button(window, text="Sentido Antihorario", command=move_counterclockwise)
+counterclockwise_button.pack()
+
+stop_button = tk.Button(window, text="Detener", command=stop_motor)
+stop_button.pack()
+
+# Función para detener el motor al cerrar la ventana
+def on_closing():
+    stop_motor()
+    GPIO.cleanup()
+    window.destroy()
+
+window.protocol("WM_DELETE_WINDOW", on_closing)
+
+window.mainloop()
