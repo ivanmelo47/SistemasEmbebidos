239fd5cdc4595355fbc80ea8613c8d27a7bff37c
ivanmelo47 <ivanmelo47@gmail.com>
2023-10-06 15:16:30 -0600

    J7

--- tkinter_steppermotor.py
+++ tkinter_steppermotor.py
@@ -1,63 +1,67 @@
 import tkinter as tk
-import RPi.GPIO as GPIO
+from RPi.GPIO import setmode, setup, output, BCM
 import time
 
-# Configura los pines GPIO
+# Configuración de pines GPIO
+setmode(BCM)
 IN1 = 2  # Pin GPIO para IN1
 IN2 = 3  # Pin GPIO para IN2
 IN3 = 4  # Pin GPIO para IN3
 IN4 = 17  # Pin GPIO para IN4
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(IN1, GPIO.OUT)
-GPIO.setup(IN2, GPIO.OUT)
-GPIO.setup(IN3, GPIO.OUT)
-GPIO.setup(IN4, GPIO.OUT)
+setup(IN1, OUT)
+setup(IN2, OUT)
+setup(IN3, OUT)
+setup(IN4, OUT)
 
-# Funciones para controlar el motor
-def girar_sentido_horario():
-    GPIO.output(IN1, GPIO.HIGH)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW)
+# Secuencia de pasos para girar el motor en sentido horario
+sequence = [
+    (1, 0, 0, 0),
+    (1, 1, 0, 0),
+    (0, 1, 0, 0),
+    (0, 1, 1, 0),
+    (0, 0, 1, 0),
+    (0, 0, 1, 1),
+    (0, 0, 0, 1),
+    (1, 0, 0, 1),
+]
 
-def girar_sentido_antihorario():
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.HIGH)
+# Función para girar el motor en la dirección deseada
+def girar_motor(pasos):
+    for _ in range(pasos):
+        for step in sequence:
+            output(IN1, step[0])
+            output(IN2, step[1])
+            output(IN3, step[2])
+            output(IN4, step[3])
+            time.sleep(0.001)  # Ajusta la velocidad aquí
 
+# Función para detener el motor
 def detener_motor():
-    GPIO.output(IN1, GPIO.LOW)
-    GPIO.output(IN2, GPIO.LOW)
-    GPIO.output(IN3, GPIO.LOW)
-    GPIO.output(IN4, GPIO.LOW)
+    output(IN1, LOW)
+    output(IN2, LOW)
+    output(IN3, LOW)
+    output(IN4, LOW)
 
 # Crear la ventana de la interfaz gráfica
 root = tk.Tk()
 root.title("Control de Motor Paso a Paso")
 
-# Funciones para controlar el motor cuando se mantienen presionados los botones
-def comenzar_giro_horario(event):
-    girar_sentido_horario()
-
-def comenzar_giro_antihorario(event):
-    girar_sentido_antihorario()
-
-def detener_giro(event):
-    detener_motor()
-
 # Crear botones para controlar el motor
-btn_horario = tk.Button(root, text="Girar Horario")
-btn_antihorario = tk.Button(root, text="Girar Antihorario")
+btn_horario = tk.Button(root, text="Sentido Horario")
+btn_antihorario = tk.Button(root, text="Sentido Antihorario")
 btn_detener = tk.Button(root, text="Detener")
 
-# Configurar las funciones de control de motor para eventos de clic
-btn_horario.bind("<ButtonPress>", comenzar_giro_horario)
-btn_horario.bind("<ButtonRelease>", detener_giro)
-btn_antihorario.bind("<ButtonPress>", comenzar_giro_antihorario)
-btn_antihorario.bind("<ButtonRelease>", detener_giro)
-btn_detener.bind("<ButtonPress>", detener_giro)
+# Configurar las funciones de los botones
+def iniciar_sentido_horario(event):
+    girar_motor(1)  # Girar un paso en sentido horario
+
+def iniciar_sentido_antihorario(event):
+    girar_motor(-1)  # Girar un paso en sentido antihorario
+
+btn_horario.bind("<ButtonPress>", iniciar_sentido_horario)
+btn_antihorario.bind("<ButtonPress>", iniciar_sentido_antihorario)
+btn_detener.bind("<ButtonPress>", detener_motor)
 
 # Colocar los botones en la ventana
 btn_horario.pack()
