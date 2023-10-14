211114025c3d3ae1a5c41fdd4cd7bf8db7353483
ivanmelo47 <ivanmelo47@gmail.com>
2023-10-14 10:32:02 -0600

    hb5

--- maquina.py
+++ maquina.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
-import dht11
+import Adafruit_DHT
 import time
 import asyncio
 
 # Configura los pines GPIO y otros valores
-pin_dht = 4  # Debes asignar el pin GPIO correcto de la Raspberry Pi
 pin_base = 23  # Debes asignar el pin GPIO correcto de la Raspberry Pi
+
+# Define el modelo del sensor y el número de pin GPIO
+sensor = Adafruit_DHT.DHT11
+pin_dht = 4  # Debes asignar el pin GPIO correcto de la Raspberry Pi
 
 # Define los pines del controlador ULN2003A
 IN1 = 17  # Debes asignar el pin GPIO correcto de la Raspberry Pi
@@ -30,9 +33,6 @@ pasos_por_vuelta = 510
 velocidad_ms = 1
 vuelta = True
 
-# Configura el sensor DHT11
-instance = dht11.DHT11(pin=pin_dht)
-
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_base, GPIO.OUT)
@@ -44,41 +44,36 @@ GPIO.setup(IN4, GPIO.OUT)
 async def control_motor():
     global vuelta
     while True:
-        result = instance.read()
-        if result.is_valid():
-            temperature = result.temperature
-        else:
-            temperature = 0
-
-        if vuelta and temperature >= 26:
-            for _ in range(pasos_por_vuelta):
-                for step in sequence_cw:
-                    GPIO.output(IN1, step[0])
-                    GPIO.output(IN2, step[1])
-                    GPIO.output(IN3, step[2])
-                    GPIO.output(IN4, step[3])
-                    await asyncio.sleep(velocidad_ms)
-            GPIO.output(pin_base, GPIO.HIGH)
-            vuelta = False
-        elif not vuelta and temperature < 26:
-            for _ in range(pasos_por_vuelta):
-                for step in sequence_ccw:
-                    GPIO.output(IN1, step[0])
-                    GPIO.output(IN2, step[1])
-                    GPIO.output(IN3, step[2])
-                    GPIO.output(IN4, step[3])
-                    await asyncio.sleep(velocidad_ms)
-            GPIO.output(pin_base, GPIO.LOW)
-            vuelta = True
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+        
+        if humidity is not None and temperature is not None:
+            if vuelta and temperature >= 26:
+                for _ in range(pasos_por_vuelta):
+                    for step in sequence_cw:
+                        GPIO.output(IN1, step[0])
+                        GPIO.output(IN2, step[1])
+                        GPIO.output(IN3, step[2])
+                        GPIO.output(IN4, step[3])
+                        await asyncio.sleep(velocidad_ms)
+                GPIO.output(pin_base, GPIO.HIGH)
+                vuelta = False
+            elif not vuelta and temperature < 26:
+                for _ in range(pasos_por_vuelta):
+                    for step in sequence_ccw:
+                        GPIO.output(IN1, step[0])
+                        GPIO.output(IN2, step[1])
+                        GPIO.output(IN3, step[2])
+                        GPIO.output(IN4, step[3])
+                        await asyncio.sleep(velocidad_ms)
+                GPIO.output(pin_base, GPIO.LOW)
+                vuelta = True
         await asyncio.sleep(1)
 
 async def lectura_sensor():
     while True:
-        result = instance.read()
-        if result.is_valid():
-            temperature = result.temperature
-            humidity = result.humidity
-            print(f"\rTemperatura: {temperature}°C, Humedad: {humidity}%", end="")
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+        if humidity is not None and temperature is not None:
+            print(f"\rTemperatura: {temperature:.2f}°C, Humedad: {humidity:.2f}%", end="")
         await asyncio.sleep(1)
 
 async def main():
