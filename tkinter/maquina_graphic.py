5ba49c224087ed7bc785d2ecf3819b05ce6c624c
ivanmelo47 <ivanmelo47@gmail.com>
2023-10-14 12:45:54 -0600

    g6

--- tkinter/maquina_graphic.py
+++ tkinter/maquina_graphic.py
@@ -1,12 +1,12 @@
 import RPi.GPIO as GPIO
 import Adafruit_DHT
+import time
 import asyncio
 import tkinter as tk
 from tkinter import ttk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import time
-import numpy as np
+import matplotlib.animation as animation
 
 # Configura los pines GPIO y otros valores
 pin_base = 19  # Debes asignar el pin GPIO correcto de la Raspberry Pi
@@ -46,94 +46,126 @@ GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
 
-# Variables para controlar la adquisición de datos
-adquirir_datos = False
-
-# Función para obtener los datos del sensor
-def obtener_datos_sensor():
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
-    return humidity, temperature
-
-# Función para mover el motor de paso
-def mover_motor():
+async def control_motor():
     global vuelta
-    if not vuelta:
-        return
-    for _ in range(pasos_por_vuelta):
-        for step in sequence_cw:
-            GPIO.output(IN1, step[0])
-            GPIO.output(IN2, step[1])
-            GPIO.output(IN3, step[2])
-            GPIO.output(IN4, step[3])
-            time.sleep(velocidad_ms)
-    GPIO.output(pin_base, GPIO.HIGH)
-    vuelta = False
-
-# Función para actualizar los gráficos
-def actualizar_grafico(i):
-    if adquirir_datos:
-        humidity, temperature = obtener_datos_sensor()
+    while True:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+        
         if humidity is not None and temperature is not None:
-            if temperature > 26:  # Agrega la condición para mover el motor
-                mover_motor()
-            xdata_temp.append(time.time())
-            ydata_temp.append(temperature)
-            xdata_hum.append(time.time())
-            ydata_hum.append(humidity)
-            if len(xdata_temp) > 20:
-                xdata_temp.pop(0)
-                ydata_temp.pop(0)
-                xdata_hum.pop(0)
-                ydata_hum.pop(0)
-            line_temp.set_data(xdata_temp, ydata_temp)
-            line_hum.set_data(xdata_hum, ydata_hum)
-            ax_temp.relim()
-            ax_temp.autoscale_view()
-            ax_hum.relim()
-            ax_hum.autoscale_view()
-            temperatura_label.config(text=f"Temperatura: {temperature:.2f}°C")
-            humedad_label.config(text=f"Humedad: {humidity:.2f}%")
+            if temperature >= 26:
+                if vuelta:
+                    for _ in range(pasos_por_vuelta):
+                        for step in sequence_cw:
+                            GPIO.output(IN1, step[0])
+                            GPIO.output(IN2, step[1])
+                            GPIO.output(IN3, step[2])
+                            GPIO.output(IN4, step[3])
+                            time.sleep(velocidad_ms)
+                    GPIO.output(pin_base, GPIO.HIGH)
+                    vuelta = False
+            else:
+                if not vuelta:
+                    for _ in range(pasos_por_vuelta):
+                        for step in sequence_ccw:
+                            GPIO.output(IN1, step[0])
+                            GPIO.output(IN2, step[1])
+                            GPIO.output(IN3, step[2])
+                            GPIO.output(IN4, step[3])
+                            time.sleep(velocidad_ms)
+                    GPIO.output(pin_base, GPIO.LOW)
+                    vuelta = True
+        await asyncio.sleep(1)
 
-# Función para iniciar la adquisición de datos
-def iniciar_adquisicion():
+# Código para crear la ventana de tkinter y los gráficos
+def crear_actualizar_grafico_temp():
+    global xdata_temp, ydata_temp, line_temp
+    xdata_temp.append(time.time())
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+    if humidity is not None and temperature is not None:
+        ydata_temp.append(temperature)
+    if len(xdata_temp) > 20:
+        xdata_temp.pop(0)
+        ydata_temp.pop(0)
+    line_temp.set_data(xdata_temp, ydata_temp)
+    ax_temp.relim()
+    ax_temp.autoscale_view()
+
+def crear_actualizar_grafico_hum():
+    global xdata_hum, ydata_hum, line_hum
+    xdata_hum.append(time.time())
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin_dht)
+    if humidity is not None and temperature is not None:
+        ydata_hum.append(humidity)
+    if len(xdata_hum) > 20:
+        xdata_hum.pop(0)
+        ydata_hum.pop(0)
+    line_hum.set_data(xdata_hum, ydata_hum)
+    ax_hum.relim()
+    ax_hum.autoscale_view()
+
+def iniciar_graficos():
     global adquirir_datos
     adquirir_datos = True
+    loop = asyncio.get_event_loop()
+    loop.create_task(control_motor())  # Llama a control_motor para mover el motor
+    ani.event_source.start()  # Inicia la animación de los gráficos
 
-# Función para detener la adquisición de datos
-def detener_adquisicion():
+def detener_graficos():
     global adquirir_datos
     adquirir_datos = False
+    ani.event_source.stop()  # Detiene la animación de los gráficos
+    loop.stop()  # Detiene el bucle de eventos de asyncio
 
-# Crear la ventana principal de Tkinter
+
 root = tk.Tk()
 root.title("Sensor DHT11 - Gráficos de Temperatura y Humedad")
 
-# Crear un marco (frame) para los gráficos de temperatura
 frame_temp = ttk.Frame(root)
 frame_temp.pack(side=tk.LEFT, padx=10, pady=10)
 frame_temp.grid(column=0, row=0)
-frame_temp.grid_propagate(0)  # Evita que el marco cambie de tamaño automáticamente
+frame_temp.grid_propagate(0)
 
-# Crear un marco (frame) para los gráficos de humedad
 frame_hum = ttk.Frame(root)
 frame_hum.pack(side=tk.LEFT, padx=10, pady=10)
 frame_hum.grid(column=1, row=0)
-frame_hum.grid_propagate(0)  # Evita que el marco cambie de tamaño automáticamente
+frame_hum.grid_propagate(0)
 
-# Crear etiquetas para mostrar los valores de temperatura y humedad
-temperatura_label = ttk.Label(frame_temp, text="", font=("Helvetica", 14))
-temperatura_label.pack()
-humedad_label = ttk.Label(frame_hum, text="", font=("Helvetica", 14))
-humedad_label.pack()
-
-# Crear botones para iniciar y detener la adquisición de datos
-iniciar_button = ttk.Button(root, text="Iniciar", command=iniciar_adquisicion)
+iniciar_button = ttk.Button(root, text="Iniciar Gráficos", command=iniciar_graficos)
 iniciar_button.pack()
-detener_button = ttk.Button(root, text="Detener", command=detener_adquisicion)
+detener_button = ttk.Button(root, text="Detener Gráficos", command=detener_graficos)
 detener_button.pack()
 
-# Crear una figura de Matplotlib con dos subgráficos en el marco de temperatura
 fig_temp = Figure(figsize=(6, 4), dpi=100)
 ax_temp = fig_temp.add_subplot(111)
 xdata_temp, ydata_temp = [], []
-line_temp
+line_temp, = ax_temp.plot(xdata_temp, ydata_temp, 'r', label="Temperatura (°C)")
+ax_temp.set_xlabel("Tiempo (s)")
+ax_temp.set_ylabel("Temperatura (°C)")
+ax_temp.set_title("Gráfico de Temperatura")
+ax_temp.legend()
+canvas_temp = FigureCanvasTkAgg(fig_temp, master=frame_temp)
+canvas_temp.get_tk_widget().pack()
+
+fig_hum = Figure(figsize=(6, 4), dpi=100)
+ax_hum = fig_hum.add_subplot(111)
+xdata_hum, ydata_hum = [], []
+line_hum, = ax_hum.plot(xdata_hum, ydata_hum, 'g', label="Humedad (%)")
+ax_hum.set_xlabel("Tiempo (s)")
+ax_hum.set_ylabel("Humedad (%)")
+ax_hum.set_title("Gráfico de Humedad")
+ax_hum.legend()
+canvas_hum = FigureCanvasTkAgg(fig_hum, master=frame_hum)
+canvas_hum.get_tk_widget().pack()
+
+adquirir_datos = False
+
+def actualizar_graficos(i):
+    if adquirir_datos:
+        crear_actualizar_grafico_temp()
+        crear_actualizar_grafico_hum()
+
+ani = animation.FuncAnimation(fig_temp, actualizar_graficos, interval=100)
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    root.mainloop()
